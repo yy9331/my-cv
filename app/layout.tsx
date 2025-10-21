b60bb5fa07e140d8e9f25ac847a950b04788b843
1 <yuyi.gz@163.com>
2025-10-22 07:29:42 +0800

    fixed Chinese export PDF font shown

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
       <body
         className={`antialiased`}
         data-theme="dark"
